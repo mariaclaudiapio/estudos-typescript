d738c220d9dbd3de44558523ab760e0f4f50f2bd
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-08-24 18:54:03 -0300

    classes - parte 02

--- classes/classes.js
+++ classes/classes.js
@@ -27,3 +27,142 @@ console.log(aniversarioEsperto);
 const casamentoEsperto = new DataEsperta; // posso omitir os parênteses
 casamentoEsperto.ano = 2017;
 console.log(casamentoEsperto);
+//Desafio classe produto
+//Atributos: nome, preço e desconto
+//Criar o construtor
+//Obs 1: o desconto é opcional (valor padrão 0)
+//Obs 2: Criar dois produtos, passando dois e três parâmetros
+class Product {
+    constructor(name, price, discount = 0) {
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+    }
+    //tb é public
+    priceWithDiscount() {
+        return this.price * (1 - this.discount);
+    }
+    summary() {
+        return `${this.name} custa R$${this.priceWithDiscount()} (${this.discount * 100}% off).`;
+    }
+}
+const product1 = new Product('Black pen', 4.20);
+product1.discount = 0.06;
+console.log(product1.summary());
+const product2 = new Product('School notebook', 18.80, 0.15);
+console.log(product2.summary());
+class Carro {
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0; //"private" é visível só na própria classe e não transmissível por herança.
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0
+            && novaVelocidade <= this.velocidadeMaxima;
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+const carro1 = new Carro('Ford', 'Ka', 185);
+Array(50).fill(0).forEach(() => carro1.acelerar());
+console.log(carro1.acelerar());
+Array(20).fill(0).forEach(() => carro1.frear());
+console.log(carro1.frear());
+class Ferrari extends Carro {
+    constructor(modelo, velocidadeMaxima) {
+        super('Ferrari', modelo, velocidadeMaxima); //o "super" deve ser chamado para que o construtor da classe pai seja executado
+    }
+    acelerar() {
+        return this.alterarVelocidade(20);
+    }
+    frear() {
+        return this.alterarVelocidade(-15);
+    }
+}
+const f40 = new Ferrari('F40', 324);
+console.log(`${f40.marca} ${f40.modelo}`);
+console.log(f40.acelerar());
+console.log(f40.frear());
+// Getters & Setters
+class Pessoa {
+    constructor() {
+        this._idade = 0;
+    }
+    get idade() {
+        return this._idade;
+    }
+    set idade(valor) {
+        if (valor >= 0 && valor <= 120) {
+            this._idade = valor;
+        }
+    }
+}
+const pessoa1 = new Pessoa;
+pessoa1.idade = 10;
+console.log(pessoa1);
+pessoa1.idade = 40;
+console.log(pessoa1.idade);
+// Atributos e métodos estáticos: o atributo estático está associado à classe.
+/* versão sem static:
+class Matematica {
+    PI: number = 3.1416
+
+    areaCirc(raio: number): number {
+        return this.PI * raio * raio
+    }
+}
+
+const m1 = new Matematica()
+m1.PI = 4.2
+console.log(m1.areaCirc(4))
+
+const m2 = new Matematica()
+console.log(m2.areaCirc(4))
+*/
+//versão com static:
+class Matematica {
+    static areaCirc(raio) {
+        return this.PI * raio * raio;
+    }
+}
+Matematica.PI = 3.1416;
+console.log(Matematica.areaCirc(4));
+// Classe abstrata
+class Calculo {
+    constructor() {
+        this.resultado = 0;
+    }
+    getResultado() {
+        return this.resultado;
+    }
+}
+class Soma extends Calculo {
+    executar(...numeros) {
+        this.resultado = numeros.reduce((t, a) => t + a);
+    }
+}
+class Multiplicacao extends Calculo {
+    executar(...numeros) {
+        this.resultado = numeros.reduce((t, a) => t * a);
+    }
+}
+let c1 = new Soma();
+c1.executar(2, 3, 4, 5);
+console.log(c1.getResultado());
+c1 = new Multiplicacao();
+c1.executar(2, 3, 4, 5);
+console.log(c1.getResultado());
