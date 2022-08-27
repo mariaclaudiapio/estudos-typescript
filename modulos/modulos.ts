ad3789e74cab78f5a4a20c670a512b285ce98d17
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-08-27 13:47:54 -0300

    módulos e namespace

---
+++ modulos/modulos.ts
@@ -0,0 +1,10 @@
+import { areaRetangulo } from "./retangulo";
+import { areaCircunferencia } from "./circunferencia";
+
+
+console.log('Modulo carregado!')
+console.log(areaRetangulo(7, 8))
+console.log(areaCircunferencia(2))
+
+const { digaOi } = require('./novo')
+console.log(digaOi('José'))
---
+++ namespaces/namespaces.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+namespace Areas {
+    const PI = 3.14
+
+    export function circunferencia(raio: number): number {
+        return PI * Math.pow(raio, 2)
+    }
+    
+    export function retangulo(base: number, altura: number): number {
+        return base * altura
+    }
+}
+
+const PI = 2.99 // esse PI definido aqui não gera conflito com o PI declarado dentro do namespace
+
+console.log(Areas.circunferencia(10))
+console.log(Areas.retangulo(10, 20))
+
+console.log(PI)
+
+// possibilidade de namespaces aninhados:
+
+namespace Geometria {
+    export namespace Area {
+        const PI = 3.14
+
+        export function circunferencia(raio: number): number {
+            return PI * Math.pow(raio, 2)
+        }
+        
+        export function retangulo(base: number, altura: number): number {
+            return base * altura
+        }
+    }
+}
+
+const PI = 2.99 // esse PI definido aqui não gera conflito com o PI declarado dentro do namespace
+
+console.log(Geometria.Area.circunferencia(10))
+console.log(Geometria.Area.retangulo(10, 20))
+
+
+///<reference path="geometriacirc.ts"/>
+///<reference path="geometriarect.ts"/>
+*/ 
