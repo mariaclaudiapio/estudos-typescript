4deb9d35949d1a27d4fa6435015af95a0a39351f
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-08-31 21:38:33 -0300

    Decorators
    parte 02

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,4 +1,5 @@
-@logarClasse
+//@logarClasse
+@logarClasseSe(false)
 class Eletrodomestico {
     constructor() {
         console.log('novo...');
@@ -7,4 +8,16 @@ class Eletrodomestico {
 
 function logarClasse(construtor: Function) {
     console.log(construtor)
+}
+
+function decoratorVazio(_: Function) {}
+
+function logarClasseSe(valor: boolean) {
+    return valor ? logarClasse: decoratorVazio
+}
+
+function decorator(a: string, b: number) {
+    return function(_: Function): void {
+        console.log(a + ' ' + b)
+    }
 }
