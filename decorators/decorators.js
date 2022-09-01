4e233933c3be6cdd81182e4dfc4f0341f98d22fd
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-09-01 19:34:08 -0300

    Decorators
    parte 03

--- decorators/decorators.js
+++ decorators/decorators.js
@@ -5,15 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-//@logarClasse
-let Eletrodomestico = class Eletrodomestico {
-    constructor() {
-        console.log('novo...');
-    }
-};
-Eletrodomestico = __decorate([
-    logarClasseSe(false)
-], Eletrodomestico);
 function logarClasse(construtor) {
     console.log(construtor);
 }
@@ -26,3 +17,26 @@ function decorator(a, b) {
         console.log(a + ' ' + b);
     };
 }
+//@logarClasse
+//@logarClasseSe(false)
+let Eletrodomestico = class Eletrodomestico {
+    constructor() {
+        console.log('novo...');
+    }
+};
+Eletrodomestico = __decorate([
+    logarObjeto
+], Eletrodomestico);
+function logarObjeto(construtor) {
+    console.log('Carregado...');
+    return class extends construtor {
+        constructor(...args) {
+            console.log('Antes...');
+            super(...args);
+            console.log('Depois...');
+        }
+    };
+}
+new Eletrodomestico();
+new Eletrodomestico();
+new Eletrodomestico();
--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,11 +1,3 @@
-//@logarClasse
-@logarClasseSe(false)
-class Eletrodomestico {
-    constructor() {
-        console.log('novo...');
-    }
-}
-
 function logarClasse(construtor: Function) {
     console.log(construtor)
 }
@@ -21,3 +13,29 @@ function decorator(a: string, b: number) {
         console.log(a + ' ' + b)
     }
 }
+
+//@logarClasse
+//@logarClasseSe(false)
+@logarObjeto
+class Eletrodomestico {
+    constructor() {
+        console.log('novo...');
+    }
+}
+
+type Construtor = { new(...args: any[]): {} }
+
+function logarObjeto(construtor: Construtor) {
+    console.log('Carregado...')
+    return class extends construtor {
+        constructor(...args: any[]) {
+            console.log('Antes...')
+            super(...args)
+            console.log('Depois...')
+        }
+    }
+}
+
+new Eletrodomestico()
+new Eletrodomestico()
+new Eletrodomestico()
